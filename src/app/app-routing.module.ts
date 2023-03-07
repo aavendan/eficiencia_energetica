92529cef9f1babc46d7e5b2b57f49b344f5651b3
aavendan <aavenda@gmail.com>
2023-03-06 20:45:02 -0500

    only forms components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,69 +10,79 @@ import { PresentationComponent } from "./pages/presentation/presentation.compone
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "presentation",
+    redirectTo: "forms/components",
     pathMatch: "full"
   },
   {
-    path: "presentation",
-    component: PresentationComponent
+      path: "",
+      component: AdminLayoutComponent,
+      children: [
+        {
+          path: "forms",
+          loadChildren: () => import('./pages/forms/forms.module').then(m => m.FormsModules)
+        },
+      ]
   },
-  {
-    path: "",
-    component: AdminLayoutComponent,
-    children: [
-      {
-        path: "dashboards",
-        loadChildren: () => import('./pages/dashboards/dashboards.module').then(m => m.DashboardsModule)
+  // {
+  //   path: "presentation",
+  //   component: PresentationComponent
+  // },
+  // {
+  //   path: "",
+  //   component: AdminLayoutComponent,
+  //   children: [
+  //     {
+  //       path: "dashboards",
+  //       loadChildren: () => import('./pages/dashboards/dashboards.module').then(m => m.DashboardsModule)
 
-      },
-      {
-        path: "components",
-        loadChildren: () => import('./pages/components/components.module').then(m => m.ComponentsModule)
-      },
-      {
-        path: "forms",
-        loadChildren: () => import('./pages/forms/forms.module').then(m => m.FormsModules)
-      },
-      {
-        path: "tables",
-        loadChildren: () => import('./pages/tables/tables.module').then(m => m.TablesModule)
-      },
-      {
-        path: "maps",
-        loadChildren: () => import('./pages/maps/maps.module').then(m => m.MapsModule)
-      },
-      {
-        path: "widgets",
-        loadChildren: () => import('./pages/widgets/widgets.module').then(m => m.WidgetsModule)
-      },
-      {
-        path: "charts",
-        loadChildren: () => import('./pages/charts/charts.module').then(m => m.ChartsModule)
-      },
-      {
-        path: "calendar",
-        loadChildren: () => import('./pages/calendar/calendar.module').then(m => m.CalendarModule)
-      },
-      {
-        path: "examples",
-        loadChildren: () => import('./pages/examples/examples.module').then(m => m.ExamplesModule)
-      }
-    ]
-  },
-  {
-    path: "",
-    component: AuthLayoutComponent,
-    children: [
-      {
-        path: "examples",
-        loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
-      }
-    ]
-  },
+  //     },
+  //     {
+  //       path: "components",
+  //       loadChildren: () => import('./pages/components/components.module').then(m => m.ComponentsModule)
+  //     },
+  //     {
+  //       path: "forms",
+  //       loadChildren: () => import('./pages/forms/forms.module').then(m => m.FormsModules)
+  //     },
+  //     {
+  //       path: "tables",
+  //       loadChildren: () => import('./pages/tables/tables.module').then(m => m.TablesModule)
+  //     },
+  //     {
+  //       path: "maps",
+  //       loadChildren: () => import('./pages/maps/maps.module').then(m => m.MapsModule)
+  //     },
+  //     {
+  //       path: "widgets",
+  //       loadChildren: () => import('./pages/widgets/widgets.module').then(m => m.WidgetsModule)
+  //     },
+  //     {
+  //       path: "charts",
+  //       loadChildren: () => import('./pages/charts/charts.module').then(m => m.ChartsModule)
+  //     },
+  //     {
+  //       path: "calendar",
+  //       loadChildren: () => import('./pages/calendar/calendar.module').then(m => m.CalendarModule)
+  //     },
+  //     {
+  //       path: "examples",
+  //       loadChildren: () => import('./pages/examples/examples.module').then(m => m.ExamplesModule)
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: "",
+  //   component: AuthLayoutComponent,
+  //   children: [
+  //     {
+  //       path: "examples",
+  //       loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
+  //     }
+  //   ]
+  // },
   {
     path: "**",
-    redirectTo: "dashboard"
+    redirectTo: "forms/components"
   }
 ];
 
