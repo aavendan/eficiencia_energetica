a6f955d384c7c442687c065c929b59a6e5973e32
Alex Velez <velezllaquealex@gmail.com>
2023-12-09 13:00:22 -0500

    Projects table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component
 import { PresentationComponent } from "./pages/presentation/presentation.component";
 
 const routes: Routes = [
-  {
-    path: "",
-    redirectTo: "eficiency/simulator",
-    pathMatch: "full"
-  },
+  // {
+  //   path: "",
+  //   redirectTo: "eficiency/simulator",
+  //   pathMatch: "full"
+  // },
   {
       path: "",
       component: AdminLayoutComponent,
@@ -21,69 +21,106 @@ const routes: Routes = [
           path: "eficiency",
           loadChildren: () => import('./pages/forms/forms.module').then(m => m.FormsModules)
         },
+        {
+          path: "dashboards",
+          loadChildren: () => import('./pages/dashboards/dashboards.module').then(m => m.DashboardsModule)
+  
+        },
+        {
+          path: "components",
+          loadChildren: () => import('./pages/components/components.module').then(m => m.ComponentsModule)
+        },
+        {
+          path: "forms",
+          loadChildren: () => import('./pages/forms/forms.module').then(m => m.FormsModules)
+        },
+        {
+          path: "tables",
+          loadChildren: () => import('./pages/tables/tables.module').then(m => m.TablesModule)
+        },
+        {
+          path: "maps",
+          loadChildren: () => import('./pages/maps/maps.module').then(m => m.MapsModule)
+        },
+        {
+          path: "widgets",
+          loadChildren: () => import('./pages/widgets/widgets.module').then(m => m.WidgetsModule)
+        },
+        {
+          path: "charts",
+          loadChildren: () => import('./pages/charts/charts.module').then(m => m.ChartsModule)
+        },
+        {
+          path: "calendar",
+          loadChildren: () => import('./pages/calendar/calendar.module').then(m => m.CalendarModule)
+        },
+        {
+          path: "examples",
+          loadChildren: () => import('./pages/examples/examples.module').then(m => m.ExamplesModule)
+        }
       ]
   },
-  // {
-  //   path: "presentation",
-  //   component: PresentationComponent
-  // },
-  // {
-  //   path: "",
-  //   component: AdminLayoutComponent,
-  //   children: [
-  //     {
-  //       path: "dashboards",
-  //       loadChildren: () => import('./pages/dashboards/dashboards.module').then(m => m.DashboardsModule)
-
-  //     },
-  //     {
-  //       path: "components",
-  //       loadChildren: () => import('./pages/components/components.module').then(m => m.ComponentsModule)
-  //     },
-  //     {
-  //       path: "forms",
-  //       loadChildren: () => import('./pages/forms/forms.module').then(m => m.FormsModules)
-  //     },
-  //     {
-  //       path: "tables",
-  //       loadChildren: () => import('./pages/tables/tables.module').then(m => m.TablesModule)
-  //     },
-  //     {
-  //       path: "maps",
-  //       loadChildren: () => import('./pages/maps/maps.module').then(m => m.MapsModule)
-  //     },
-  //     {
-  //       path: "widgets",
-  //       loadChildren: () => import('./pages/widgets/widgets.module').then(m => m.WidgetsModule)
-  //     },
-  //     {
-  //       path: "charts",
-  //       loadChildren: () => import('./pages/charts/charts.module').then(m => m.ChartsModule)
-  //     },
-  //     {
-  //       path: "calendar",
-  //       loadChildren: () => import('./pages/calendar/calendar.module').then(m => m.CalendarModule)
-  //     },
-  //     {
-  //       path: "examples",
-  //       loadChildren: () => import('./pages/examples/examples.module').then(m => m.ExamplesModule)
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: "",
-  //   component: AuthLayoutComponent,
-  //   children: [
-  //     {
-  //       path: "examples",
-  //       loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
-  //     }
-  //   ]
-  // },
   {
-    path: "**",
-    redirectTo: "forms/components"
-  }
+    path: "presentation",
+    component: PresentationComponent
+  },
+  {
+    path: "",
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: "dashboards",
+        loadChildren: () => import('./pages/dashboards/dashboards.module').then(m => m.DashboardsModule)
+
+      },
+      {
+        path: "components",
+        loadChildren: () => import('./pages/components/components.module').then(m => m.ComponentsModule)
+      },
+      {
+        path: "forms",
+        loadChildren: () => import('./pages/forms/forms.module').then(m => m.FormsModules)
+      },
+      {
+        path: "tables",
+        loadChildren: () => import('./pages/tables/tables.module').then(m => m.TablesModule)
+      },
+      {
+        path: "maps",
+        loadChildren: () => import('./pages/maps/maps.module').then(m => m.MapsModule)
+      },
+      {
+        path: "widgets",
+        loadChildren: () => import('./pages/widgets/widgets.module').then(m => m.WidgetsModule)
+      },
+      {
+        path: "charts",
+        loadChildren: () => import('./pages/charts/charts.module').then(m => m.ChartsModule)
+      },
+      {
+        path: "calendar",
+        loadChildren: () => import('./pages/calendar/calendar.module').then(m => m.CalendarModule)
+      },
+      {
+        path: "examples",
+        loadChildren: () => import('./pages/examples/examples.module').then(m => m.ExamplesModule)
+      }
+    ]
+  },
+  {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "examples",
+        loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
+      }
+    ]
+  },
+  // {
+  //   path: "**",
+  //   redirectTo: "forms/components"
+  // }
 ];
 
 @NgModule({
